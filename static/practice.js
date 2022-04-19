cc17cc2a08599b9904b0d358afe7914d1fed5c23
Name <carolinewang959@gmail.com>
2022-04-18 21:46:47 -0400

    saving user answers to practice question

--- static/practice.js
+++ static/practice.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("button").click(function(){
+    $(".next").click(function(){
         if(lesson["next_lesson"]!= "end"){
             let newUrl = "/learn/"+lesson["next_lesson"]
             window.location.href = newUrl
@@ -8,6 +8,30 @@ $(document).ready(function(){
             let newUrl = "/quiz/1"
             window.location.href = newUrl
         }
+        
+    })
+    $(".check").click(function(){
+        console.log("check")
+        let response = {
+            "practice_number": parseInt(lesson_id),
+            "answer": $("textarea").val()
+        };
+        $.ajax({
+            type: "GET",
+            url: "/add_practice_result",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(response),
+            success: function(result){
+                console.log("success")
+            },
+            error: function(request, status, error){
+                console.log("Error")
+                console.log(request)
+                console.log(status)
+                console.log(error)
+            }
+        });
         
     })
 })
